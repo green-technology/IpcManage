d3474705adc8b8b546e166543d3acead4d20e072
workai <azlgoole@gmail.com>
2015-09-05 11:57:55 +0800

    使用SDK连接IPC

--- rpc/gen-cpp/IpcManageTools.cpp
+++ rpc/gen-cpp/IpcManageTools.cpp
@@ -1,2 +1,59 @@
 #include "IpcManageTools.h"
+#include <Windows.h>
+#include "dh\dhnetsdk.h"
+#include "hc\HCNetSDK.h"
+#include <assert.h>
 
+
+namespace ipcTools
+{
+	ConnectManager* ConnectManager::Instance()
+	{
+		static ConnectManager* instance = NULL;
+		if(!instance)
+			instance = new ConnectManager();
+
+		assert(instance);
+
+		return instance;
+	}
+
+	long ConnectManager::connectDVR(const ConnectInfo &DVRInfo)
+	{
+		map<ConnectInfo, long>::iterator iter = m_ConnectList.find(DVRInfo);
+		if(iter == m_ConnectList.end())
+			return iter->second;
+
+		char ip[16] = {0};
+		strcpy_s(ip, DVRInfo.ip);
+		char userName[100];
+		strcpy_s(userName, DVRInfo.userName.c_str());
+		char password[100];
+		strcpy_s(password, DVRInfo.password.c_str());
+
+		long ret = NULL;
+		switch(DVRInfo.type)
+		{
+		case DeviceTypeDH:
+			{
+				int err = 0;
+				NET_DEVICEINFO info;// 用于返回设备信息
+				memset(&info, 0 ,sizeof(NET_DEVICEINFO));
+				long ret = CLIENT_Login(ip, DVRInfo.port, userName, password, &info, &err);	// 登录并返回设备信息
+			}
+			break;
+		case DeviceTypeHC:
+			{
+				NET_DVR_DEVICEINFO_V30 info;
+				memset(&info,0,sizeof(NET_DVR_DEVICEINFO_V30));
+				long ret = NET_DVR_Login_V30(ip, DVRInfo.port, userName, password, &info);
+			}
+			break;
+		default:
+			assert(false);
+			break;
+		}
+		return ret;
+	}
+
+}
--- rpc/gen-cpp/IpcManageTools.h
+++ rpc/gen-cpp/IpcManageTools.h
@@ -1,6 +1,68 @@
 #ifndef IpcManageTools_H
 #define IpcManageTools_H
 
+#include <string>
+#include <map>
+
+using namespace std;
+
+namespace ipcTools
+{
+	enum DeviceType
+	{
+		DeviceTypeHC,
+		DeviceTypeDH
+	};
+
+	class ConnectInfo
+	{
+	public:
+		char ip[16];
+		int port;
+		DeviceType type;
+		string userName;
+		string password;
+		int channel;
+
+		friend bool operator == (const ConnectInfo &lhs, const ConnectInfo &rhs)
+		{
+			bool ret = false;
+			if(strcmp(lhs.ip, rhs.ip) == 0 && lhs.port == rhs.port)
+				ret = true;
+			return ret;
+		}
+
+		friend bool operator < (const ConnectInfo &lhs, const ConnectInfo &rhs)
+		{
+			bool ret = false;
+			int cmp = strcmp(lhs.ip, rhs.ip);
+			if (cmp < 0)
+				ret = true;
+			else if (ret == 0 && lhs.port < rhs.port)
+				ret = true;
+			return ret;
+		}
+	};
+
+	class ConnectManager
+	{
+	public:
+		ConnectManager* Instance();
+
+		long connectDVR(const ConnectInfo &info);
+
+	private:
+		ConnectManager() {};
+
+		long connectHCDVR(const ConnectInfo &info);
+		long connectDHDVR(const ConnectInfo &info);
+
+	private:
+		map<ConnectInfo, long> m_ConnectList;
+
+	};
+
+}
 
 
 #endif
