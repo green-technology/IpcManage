032911bb1cb4085dde7d8603712021cf6c2d1cfd
workai <azlgoole@gmail.com>
2015-10-25 13:15:06 +0800

    整理

--- rpc/gen-cpp/log.cpp
+++
@@ -1,9 +0,0 @@
-#include "log.h"
-#include "glog\logging.h"
-
-void InitLogging(const char* argv0)
-{
-	google::InitGoogleLogging(argv0);
-}
-
-//google::LogMessage(__FILE__, __LINE__, google::GLOG_INFO, counter, &google::LogMessage::SendToLog);
